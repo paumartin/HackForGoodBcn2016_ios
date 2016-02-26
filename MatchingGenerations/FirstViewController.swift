15c7f0b49414dc9d957965b7f68e4bcf902703ea
Oscar <oscarbc@me.com>
2016-02-26 13:52:18 +0100

    Initial Commit

---
+++ MatchingGenerations/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MatchingGenerations
+//
+//  Created by Oscar Blanco Castan on 26/2/16.
+//  Copyright © 2016 AlcaponeTeam. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MatchingGenerations/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MatchingGenerations
+//
+//  Created by Oscar Blanco Castan on 26/2/16.
+//  Copyright © 2016 AlcaponeTeam. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
